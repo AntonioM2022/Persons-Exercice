8f9bee451171d8216e32148bb728ddedc6c70f08
Antonio M <kirye2018@gmail.com>
2025-02-03 20:44:45 -0600

    HotFix

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const morgan = require('morgan')
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
+require('dotenv').config()
+const Person = require('./models/person')
+//const person = require('./models/person')
 
 
 //aqui es como si crearamos una variable para ponerla en el formato de abajo, se crea la variable body que lo que tiene es la
@@ -38,7 +41,9 @@ let persons= [
 ]
 
 app.get('/api/persons',(request, response)=>{
+  Person.find({}).then(persons=>{
     response.json(persons)
+  })
 })
 
 app.get('/info',(request, response)=>{
@@ -61,23 +66,27 @@ app.get('/info',(request, response)=>{
     response.send(`<p>Phonebook has info for ${entrances} people</p><p>${time}</p>`)
 })
 
-app.get('/api/persons/:id', (request, response)=>{
-    const id= Number(request.params.id)
-    const person= persons.find(p=>p.id===id)
-    if(person){
+app.get('/api/persons/:id', (request, response, next)=>{
+    Person.findById(request.params.id)
+    .then(person=>{
+      if(person){
         response.json(person)
     }else{
         response.status(404).end()
     }
+    })
+    .catch(error=> next(error))
 })
 
-app.delete('/api/persons/:id', (request, response)=>{
-    const id= Number(request.params.id)
-    persons= persons.filter(person=> person.id!==id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next)=>{
+    Person.findByIdAndDelete(request.params.id)
+    .then(result=>{
+      response.status(204).end()
+    })
+    .catch(error=> next(error))
 })
 
-app.post('/api/persons',(request, response)=>{
+app.post('/api/persons',(request, response, next)=>{
     const body= request.body
     
     if (!body.name || !body.number){
@@ -85,17 +94,51 @@ app.post('/api/persons',(request, response)=>{
             error: 'name or number misssing'
         })
       }else{
-        const newId= Math.floor(Math.random()*1000)
-        const newPerson= {
-            id: newId,
+        const newPerson= new Person({
             name: body.name,
             number: body.number
-        }
-        persons.concat(newPerson)
-        response.json(newPerson)
+        })
+        newPerson.save()
+        .then(savedPerson=>{
+          response.json(savedPerson)
+        })
+        .catch(error=> next(error))
       }
     
 })
+
+app.put('/api/persons/:id', (request, response, next)=>{
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(request.params.id, {name, number}, {new: true, runValidators: true, context: 'query', validate: 'validator'})
+  .then(updatedPerson=>{
+    response.json(updatedPerson)
+  })
+  .catch(error=> next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// controlador de solicitudes con endpoint desconocido
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: error.message })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
+
 
 
 const PORT = process.env.PORT || 3001
